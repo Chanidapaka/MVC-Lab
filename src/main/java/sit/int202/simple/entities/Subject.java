b36f7110e4e17fdc808f6cd0403544afb5a29fea
Chanidapaka <chanidapapanta@gmail.com>
2024-10-27 14:21:03 +0700

    first commit

---
+++ src/main/java/sit/int202/simple/entities/Subject.java
@@ -0,0 +1,12 @@
+package sit.int202.simple;
+import lombok.*;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Subject {
+    private String id;
+    private String title;
+    private double credit;
+}
